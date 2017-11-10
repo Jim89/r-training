fcd0d42db7a59bb9ac22a4900a899bd3038b5d88
Jim Leach <jim.leach@kpmg.co.uk>
2017-11-10 11:36:28 +0000

    Add simple script to make it easier to SCP the HTML files and supporting dirs to the shiny server

---
+++ move-files.sh
@@ -0,0 +1,11 @@
+# check parameters
+if [ -z "$1" ]; then
+  echo "Please specify a user"
+  exit 1
+fi
+
+# Set the user value from the input value (just for convenience and ease of reading the code)
+user="$1"
+
+# scp the files to the shiny server - should be the HTML files and supporting _files, _cache and www directories
+scp -r *.html *_files *_cache www/ ${user}@ukixeapp208.uk.kworld.kpmg.com:/srv/shiny-server/r-training
